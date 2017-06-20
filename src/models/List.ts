8a575b8e4abf1fcfe0978b0eeebe8ca7ff50a7f5
MaxMEllon <maxmellon1994@gmail.com>
2017-06-21 05:13:13 +0900

    Rename valuable

--- src/models/List.ts
+++ src/models/List.ts
@@ -5,40 +5,40 @@ type callback = (iterator: T) => any;
 type each = (iterator: T) => void | any;
 
 export default class List<T> {
-  private self: T[];
+  private raw: T[];
 
   constructor() {
-    this.self = [];
+    this.raw = [];
   }
 
   add(obj: T): void {
-    this.self.push(obj);
+    this.raw.push(obj);
   }
   
   each(func: each): void {
-    this.self.forEach(i => func(i));
+    this.raw.forEach(i => func(i));
   }
 
   map(func: callback): R[] {
-    return this.self.map(i => func(i));
+    return this.raw.map(i => func(i));
   }
 
   remove(id: number): void {
-    delete this.self[id];
+    delete this.raw[id];
   }
 
   find(id: number): T {
-    return this.self[id];
+    return this.raw[id];
   }
 
   where(scope: where): T[] {
-    return this.self.filter(i => scope(i));
+    return this.raw.filter(i => scope(i));
   }
 
 
   clone() {
     const c = new List<T>();
-    c.self = this.self;
+    c.raw = this.raw;
     return c;
   }
 }
