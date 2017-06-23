b96ce2b9c8c33d4abc6f1088eba5bc8552f6f156
MaxMEllon <maxmellon1994@gmail.com>
2017-06-24 01:16:50 +0900

    Modify entry points

--- entrypoints/main.dev.js
+++ entrypoints/main.dev.js
@@ -2,10 +2,19 @@ const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
-app.on('ready',  () => {
-  mainWindow = new BrowserWindow({ width: 800,  height: 800 });
-  mainWindow.loadURL('http://localhost:3000');
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 800,
+    frame: false,
+    show: true,
+    transparent: true,
+    resizable: false,
+    alwaysOnTop: true,
+  });
   mainWindow.on('closed', () => (mainWindow = null));
+  mainWindow.maximize();
+  mainWindow.loadURL('http://localhost:3000');
 });
 
 app.on('window-all-closed', () => app.quit());
