49214f3755295f5dcbcdfb1938b8ea9c682611aa
MaxMEllon <maxmellon1994@gmail.com>
2017-06-25 17:11:29 +0900

    Fix typo

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -2,17 +2,11 @@ import * as React from 'react'
 import CommentList from './CommentList'
 import DebugUI from './DebugUI'
 
-export default class App extends React.Component<any, any> {
-  constructor (props) {
-    super(props)
-  }
-
-  render () {
-    return (
-      <div>
-        <CommentList />
-        <DebugUI />
-      </div>
-    )
-  }
+export default function App () {
+  return (
+    <div>
+      <CommentList />
+      <DebugUI />
+    </div>
+  )
 }
